5351902ef16d43e6df960e071905c6a5d0d2e1d5
Guoli Lyu <guoli-lv@hotmail.com>
2020-06-24 10:16:18 +0800

    chore: migrate CI from travis to github workflows

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! **Why bother writing similar code twice for blocking and async code?**
 //!
-//! [![Build Status](https://travis-ci.com/fMeow/maybe-async-rs.svg?token=WSHqSm6F65Fza985QMqn&branch=master)](https://travis-ci.com/fMeow/maybe-async-rs)
+//! [![Build Status](https://github.com/fMeow/maybe-async-rs/workflows/CI%20%28Linux%29/badge.svg?branch=master)](https://github.com/fMeow/maybe-async-rs/actions)
 //! [![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
 //! [![Latest Version](https://img.shields.io/crates/v/maybe-async.svg)](https://crates.io/crates/maybe-async)
 //! [![maybe-async](https://docs.rs/maybe-async/badge.svg)](https://docs.rs/maybe-async)
