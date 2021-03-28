35d50f8f2a7e2400a6ddc01625d1aea62d401557
Guoli Lyu <guoli-lv@hotmail.com>
2021-03-28 17:10:20 +0800

    test: replace generic type of Future with Output

--- tests/test.rs
+++ tests/test.rs
@@ -5,6 +5,8 @@ fn ui() {
     t.pass("tests/ui/02-must-be-async.rs");
     t.pass("tests/ui/03-must-be-sync.rs");
     t.pass("tests/ui/04-unit-test-util.rs");
+    t.pass("tests/ui/05-replace-future-generic-type-with-output.rs");
+
     t.compile_fail("tests/ui/test_fail/01-empty-test.rs");
     t.compile_fail("tests/ui/test_fail/02-unknown-path.rs");
     t.compile_fail("tests/ui/test_fail/03-async-gt2.rs");
