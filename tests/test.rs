51cf5b74d919edb798e125c13f22808af9fec3db
Guoli Lyu <guoli-lv@hotmail.com>
2021-03-28 17:10:52 +0800

    test: sync impl and async impl

--- tests/test.rs
+++ tests/test.rs
@@ -6,6 +6,7 @@ fn ui() {
     t.pass("tests/ui/03-must-be-sync.rs");
     t.pass("tests/ui/04-unit-test-util.rs");
     t.pass("tests/ui/05-replace-future-generic-type-with-output.rs");
+    t.pass("tests/ui/06-sync_impl_async_impl.rs");
 
     t.compile_fail("tests/ui/test_fail/01-empty-test.rs");
     t.compile_fail("tests/ui/test_fail/02-unknown-path.rs");
