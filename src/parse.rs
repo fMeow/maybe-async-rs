183d7b09b06465156bd80dad174321bbc0267921
qiuchengxuan <qiuchengxuan@gmail.com>
2022-02-26 23:40:29 +0800

    fix: applying to pub(crate) trait fail

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 use proc_macro2::Span;
 use syn::{
-    parse::{Parse, ParseStream, Result},
-    Attribute, Error, ItemFn, ItemImpl, ItemTrait, Token,
+    parse::{discouraged::Speculative, Parse, ParseStream, Result},
+    Attribute, Error, ItemFn, ItemImpl, ItemTrait,
 };
 
 pub enum Item {
@@ -9,43 +9,37 @@ pub enum Item {
     Impl(ItemImpl),
     Fn(ItemFn),
 }
+
+macro_rules! fork {
+    ($fork:ident = $input:ident) => {{
+        $fork = $input.fork();
+        &$fork
+    }};
+}
+
 impl Parse for Item {
     fn parse(input: ParseStream) -> Result<Self> {
         let attrs = input.call(Attribute::parse_outer)?;
-        let mut lookahead = input.lookahead1();
-        if lookahead.peek(Token![unsafe]) {
-            let ahead = input.fork();
-            ahead.parse::<Token![unsafe]>()?;
-            lookahead = ahead.lookahead1();
-        }
-        if lookahead.peek(Token![impl]) {
-            let mut item: ItemImpl = input.parse()?;
+        let mut fork;
+        let item = if let Some(mut item) = fork!(fork = input).parse::<ItemImpl>().ok() {
             if item.trait_.is_none() {
                 return Err(Error::new(Span::call_site(), "expected a trait impl"));
             }
             item.attrs = attrs;
-            Ok(Item::Impl(item))
-        } else if lookahead.peek(Token![pub])
-            || lookahead.peek(Token![trait])
-            || lookahead.peek(Token![fn])
-            || lookahead.peek(Token![async])
-        {
-            if lookahead.peek(Token![pub]) {
-                let ahead = input.fork();
-                ahead.parse::<Token![pub]>()?;
-                lookahead = ahead.lookahead1();
-            }
-            if lookahead.peek(Token![trait]) {
-                let mut item: ItemTrait = input.parse()?;
-                item.attrs = attrs;
-                Ok(Item::Trait(item))
-            } else {
-                let mut item: ItemFn = input.parse()?;
-                item.attrs = attrs;
-                Ok(Item::Fn(item))
-            }
+            Item::Impl(item)
+        } else if let Some(mut item) = fork!(fork = input).parse::<ItemTrait>().ok() {
+            item.attrs = attrs;
+            Item::Trait(item)
+        } else if let Some(mut item) = fork!(fork = input).parse::<ItemFn>().ok() {
+            item.attrs = attrs;
+            Item::Fn(item)
         } else {
-            Err(lookahead.error())
-        }
+            return Err(Error::new(
+                Span::call_site(),
+                "expected trait impl, trait or fn",
+            ));
+        };
+        input.advance_to(&fork);
+        Ok(item)
     }
 }
