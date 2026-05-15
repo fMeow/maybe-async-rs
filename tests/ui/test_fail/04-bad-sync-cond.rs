5b709ac4a04f4b3f1a29100f8c33fd02cddc4154
fMeow <guoli-lv@hotmail.com>
2026-05-15 13:31:42 +0800

    test: fix test error for bad sync cond

--- tests/ui/test_fail/04-bad-sync-cond.rs
+++ tests/ui/test_fail/04-bad-sync-cond.rs
@@ -1,16 +1,6 @@
-use maybe_async::maybe_async;
-
-#[maybe_async]
-async fn async_fn() -> bool {
-    true
-}
-
 // bad sync condition
 #[maybe_async::test(unknown(feature="async", async_std::test))]
-async fn test_async_fn() {
-    let res = async_fn().await;
-    assert_eq!(res, true);
-}
+async fn test_async_fn() {}
 
 fn main() {
 
